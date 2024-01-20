1acd1103673e1ef5a9c4df591c7813e38446421a
byronnDev <mikel.e.g.2004@gmail.com>
2024-01-20 23:40:11 +0100

    Add validation for mandatory name field in create monitor endpoint

--- src/Controller/MonitorController.php
+++ src/Controller/MonitorController.php
@@ -61,13 +61,17 @@ class MonitorController extends AbstractController
         $json = json_decode($request->getContent()); // Returns an object with the data
 
         // Consult the database to check if the monitor already exists
-        $monitorInput = $entityManager->getRepository(Monitor::class)->findBy(['name' => $json->name]);
+        if (property_exists($json, 'name')) {
+            $monitorInput = $entityManager->getRepository(Monitor::class)->findBy(['name' => $json->name]);
+        } else {
+            return new JsonResponse(['code' => 21, 'description' => 'The name is mandatory'], JsonResponse::HTTP_BAD_REQUEST);
+        }
 
         // Return a JSON response with data and 409 status code if the monitor already exists
         if ($monitorInput) {
-            return new JsonResponse(['code' => 21, 'description' => 'Monitor already exists'], JsonResponse::HTTP_CONFLICT);
+            return new JsonResponse(['code' => 22, 'description' => 'The monitor already exists'], JsonResponse::HTTP_CONFLICT);
         }
-        
+
         // Create a new monitor object and set its data from the received JSON
         $monitor = new Monitor();
         $monitor->setName($json->name);
@@ -80,7 +84,7 @@ class MonitorController extends AbstractController
 
         if (count($errors) > 0) {
             $logger->error('Validation error: ' . $errors);
-            return new JsonResponse(['code' => 21, 'description' => (string) $errors], JsonResponse::HTTP_BAD_REQUEST);
+            return new JsonResponse(['code' => 23, 'description' => (string) $errors], JsonResponse::HTTP_BAD_REQUEST);
         }
 
         // Save the new monitor into the database
@@ -97,6 +101,69 @@ class MonitorController extends AbstractController
         ];
 
         // Return a JSON response with the created monitor and 200 status code
+        return new JsonResponse($monitorAsArray, JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * @Route("/monitors/{monitorId}", name="update_monitor", methods={"PUT"})
+     */
+    public function update(int $monitorId, Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger, ValidatorValidatorInterface $validator): JsonResponse
+    {
+        /* 
+        JSON example value for the request body:
+        {
+            "id": 10,
+            "name": "Miguel Goyena",
+            "email": "miguel_goyena@cuatrovientos.org",
+            "phone": "654767676",
+            "photo": "http://foto.com/miguel.goyena"
+        } */
+        // Get the JSON string from the request body
+        $logger->debug('Request: ' . $request->getContent());
+        $json = json_decode($request->getContent()); // Returns an object with the data
+
+        // Consult the database to check if the monitor already exists
+        $monitor = $entityManager->getRepository(Monitor::class)->findBy(['id' => $monitorId]);
+
+        if (!$monitor) {
+            return new JsonResponse(['code' => 20, 'description' => 'Monitor not found'], JsonResponse::HTTP_CONFLICT);
+        }
+
+        if (!property_exists($json, 'name')) {
+            return new JsonResponse(['code' => 21, 'description' => 'The name is mandatory'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Update the monitor object with the received JSON data
+        if (is_object($json)) {
+            $monitor[0]->setName($json->name);
+            $monitor[0]->setEmail($json->email);
+            $monitor[0]->setPhone($json->phone);
+            $monitor[0]->setPhoto($json->photo);
+        } else {
+            // Handle the case when $json is not an object
+            return new JsonResponse(['code' => 24, 'description' => 'Invalid JSON data'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Validate the new monitor object
+        $errors = $validator->validate($monitor);
+
+        if (count($errors) > 0) {
+            $logger->error('Validation error: ' . $errors);
+            return new JsonResponse(['code' => 23, 'description' => (string) $errors], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Save the new monitor into the database
+        $entityManager->flush();
+
+        // Convert the object into an associative array
+        $monitorAsArray = [
+            'id' => $monitorId,
+            'name' => $monitor[0]->getName(),
+            'email' => $monitor[0]->getEmail(),
+            'phone' => $monitor[0]->getPhone(),
+            'photo' => $monitor[0]->getPhoto(),
+        ];
+
         return new JsonResponse($monitorAsArray, JsonResponse::HTTP_OK);
     }
 }
