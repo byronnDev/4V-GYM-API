ab7f7ca61c47880e019e2876a221d2c6eec20ce4
byronnDev <mikel.e.g.2004@gmail.com>
2024-01-20 23:58:11 +0100

    Add getAll method to ActivityController

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,15 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Activity;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ActivityController extends AbstractController
 {
     /**
      * @Route("/activities", name="get_activities", methods={"GET"})
      */
-    public function getAll(): JsonResponse
+    public function getAll(EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
     {
+        $activities = $entityManager->getRepository(Activity::class)->findAll();
 
-        return new JsonResponse(['status' => 'ok']);
+        // Validate the entity
+        $errors = $validator->validate($activities);
+
+        // Return a JSON response¡ with no data and 404 status code
+        if (count($errors) > 0) {
+            return new JsonResponse('Any problem in the Server', JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Convert the object into an associative array with the help of a foreach loop
+        $activitiesAsArray = [];
+        foreach ($activities as $activity) {
+            $activitiesAsArray[] = [
+                'id' => $activity->getId(),
+                'activity_type' => $activity->getActivityType(),
+                'monitors' => $activity->getMonitors(),
+                'date_start' => $activity->getDateStart(),
+                'date_end' => $activity->getDateEnd()
+            ];
+        }
+
+        // Return a JSON response with the found activities and 200 status code
+        return new JsonResponse($activitiesAsArray, JsonResponse::HTTP_OK);
     }
 }
